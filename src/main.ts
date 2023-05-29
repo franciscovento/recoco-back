d48f0b692fe6c743501d9ab7c986b31a20b50aff
fran <francisco.vento@outlook.com>
2023-05-28 23:42:31 -0300

    feat: adding swagger to project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
+  const config = new DocumentBuilder()
+    .setTitle('Recoco')
+    .setDescription('Recoco API')
+    .setVersion('0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1/docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
