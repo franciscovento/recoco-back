32f28d3661df4a13960c0ca8d74d232cfd6fc3ba
fran <francisco.vento@outlook.com>
2023-06-03 19:18:22 -0300

    feat: addings countries seeder and get funcion

---
+++ prisma/seed.ts
@@ -0,0 +1,52 @@
+// seeds.ts
+
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const americaCountries = [
+    { name: 'Argentina' },
+    { name: 'Bolivia' },
+    { name: 'Brazil' },
+    { name: 'Canada' },
+    { name: 'Chile' },
+    { name: 'Colombia' },
+    { name: 'Costa Rica' },
+    { name: 'Cuba' },
+    { name: 'Dominican Republic' },
+    { name: 'Ecuador' },
+    { name: 'El Salvador' },
+    { name: 'Guatemala' },
+    { name: 'Haiti' },
+    { name: 'Honduras' },
+    { name: 'Jamaica' },
+    { name: 'Mexico' },
+    { name: 'Nicaragua' },
+    { name: 'Panama' },
+    { name: 'Paraguay' },
+    { name: 'Peru' },
+    { name: 'Puerto Rico' },
+    { name: 'United States' },
+    { name: 'Uruguay' },
+    { name: 'Venezuela' },
+    // Agrega más países de América según sea necesario
+  ];
+
+  for (const country of americaCountries) {
+    await prisma.country.create({
+      data: {
+        name: country.name,
+      },
+    });
+  }
+}
+
+main()
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { UniversityModule } from './university/university.module';
+import { CountryModule } from './country/country.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule, UniversityModule],
+  imports: [PrismaModule, UserModule, AuthModule, UniversityModule, CountryModule],
 })
 export class AppModule {}
