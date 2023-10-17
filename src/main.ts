f5f9fd9e55c6f30bea2075eb1b5adb1f0337bd44
fran <francisco.vento@outlook.com>
2023-10-17 19:28:20 -0300

    fix: changue port to env

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from './validation.pipe';
 
 async function bootstrap() {
+  const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe());
@@ -15,6 +16,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/v1/docs', app, document);
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
