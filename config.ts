84abb8cd52348505f1bf928f773d73184ffe135e
fran <francisco.vento@outlook.com>
2023-12-08 20:45:10 -0300

    fix: adding url to cors

--- config.ts
+++ config.ts
@@ -1,11 +1,19 @@
 export const corsConfig = {
   development: {
-    origin: ['http://localhost:3000', 'https://recoco-front-v2.vercel.app'],
+    origin: [
+      'http://localhost:3000',
+      'https://recoco-front-v2.vercel.app',
+      'https://recoco.pro',
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   },
   production: {
-    origin: ['http://localhost:3000', 'https://recoco-front-v2.vercel.app'],
+    origin: [
+      'http://localhost:3000',
+      'https://recoco-front-v2.vercel.app',
+      'https://recoco.pro',
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   },
